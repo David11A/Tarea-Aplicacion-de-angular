994cbe436910ab7043d21c53eca9a1ef24a93f7d
DAVID11 <93211987+David11A@users.noreply.github.com>
2024-02-29 22:21:24 -0600

    commit

---
+++ my-angular/src/app/app.component.ts
@@ -0,0 +1,13 @@
+import { Component } from '@angular/core';
+import { RouterOutlet } from '@angular/router';
+
+@Component({
+  selector: 'app-root',
+  standalone: true,
+  imports: [RouterOutlet],
+  templateUrl: './app.component.html',
+  styleUrl: './app.component.css'
+})
+export class AppComponent {
+  MensajeQuienesSomos = 'En Servicios Especializados en Seguridad Privada S.DE R.L, nos enorgullece ser una empresa hondureña comprometida con la protección de nuestros clientes y sus bienes más preciados. Con una sólida experiencia en el sector, nos destacamos por ofrecer soluciones de seguridad integrales y personalizadas que se adaptan a las necesidades específicas de cada cliente';
+}
